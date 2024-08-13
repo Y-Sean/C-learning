b4c0a161a70fd481e0eb0e8fbe9a0952ee2db0ae
y3329642618Ms <2244799375@qq.com>
2024-08-13 16:32:34 +0800

    C代码练习

---
+++ test_8_11/test_8_11/1.c
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//KiKi学会了printf在屏幕输出信息，他想输出一架小飞机。请帮他编写程序输出这架小飞机。
+//#include<stdio.h>
+//int main()
+//{
+//	printf("     **     \n");
+//	printf("     **     \n");
+//	printf("************\n");
+//	printf("************\n");
+//	printf("    *  *    \n");
+//	printf("    *  *    \n");
+//	return 0;
+//}
+
+//据说智商140以上者称为天才，KiKi想知道他自己是不是天才，请帮他编程判断。输入一个整数表示一个人的智商，如果大于等于140，则表明他是一个天才，输出“Genius”。
+//输入描述：
+//多组输入，每行输入包括一个整数表示的智商。
+//输出描述：
+//针对每行输入，输出“Genius”。
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	if (a >= 140)
+//		printf("Genius\n");
+//	return 0;
+//}
+
+//输出描述：
+//I lost my cellphone!
+//#include<stdio.h>
+//int main()
+//{
+//	printf("I lost my cellphone!\n");
+//	return 0;
+//}
+
+
+//请计算表达式“(-8+22)×a-10+c÷2”，其中，a = 40，c = 212。
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 40, c = 212;
+//	printf("%d\n", (-8 + 22) * a - 10 + c / 2);
+//	return 0;
+//}
---
+++ test_8_13/test_8_13/2.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//判断一个整数是否能5整除
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	if (a % 5 == 0)
+//		printf("YES\n");
+//	else
+//		printf("NO\n");
+//	return 0;
+//}
+
+//已知一个函数y = f(x)，当x < 0时，y = 1；当x = 0时，y = 0；当x > 0时，y = -1。
+//#include<stdio.h>
+//int main()
+//{
+//	int x = 0, y = 0;
+//	scanf("%d",& x);
+//	if (x == 0)
+//		printf("0\n");
+//	else if (x > 0)
+//		printf("-1\n");
+//	else
+//		printf("1\n");
+//	return 0;
+//}
+
+
+
+
+//给定两个整数a和b(0 < a, b < 10, 000)，计算a除以b的整数商和余数。
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0, b = 0;
+//	scanf("%d%d", &a, &b);
+//	printf("%d %d", a / b, a % b);
+//	return 0;
+//}
